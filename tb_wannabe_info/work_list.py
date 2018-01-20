513086f9b830f6337f69c8f755300a84e9ab7ed0
Pierre Marijon <pierre@marijon.fr>
2018-01-20 22:06:29 +0100

    better work list with feminisation of work

--- tb_wannabe_info/work_list.py
+++ tb_wannabe_info/work_list.py
@@ -1,4 +1,10 @@
-work_list = [
-    "informaticien.ne",
-    "agriculteur.ice"
-]
+import os
+import csv
+import tb_wannabe_info
+
+work_list = list()
+with open(os.path.dirname(tb_wannabe_info.__file__) + "/liste_metier.csv") as f:
+    reader = csv.reader(f)
+    for l in reader:
+        work_list.append(tuple(l))
+
