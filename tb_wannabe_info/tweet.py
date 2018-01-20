d5fe74ca1425092275879cb25125979420bc08ea
Pierre Marijon <pierre@marijon.fr>
2018-01-21 00:04:02 +0100

    Now each element of list have same probability to be tweet

--- tb_wannabe_info/tweet.py
+++ tb_wannabe_info/tweet.py
@@ -22,37 +22,48 @@ ends = ('informaticien', 'informaticienne')
 
 order = [(1, 0), (0, 1)]
 
+hastags = ["#infomatique", "#digital", "#numerique", "#travail", "#changement",
+           "#bot"]
+
 def loop(api, sleep_time):
+    random.shuffle(work_list)
+    i = 0
     while True:
-        try:
-            work = random.choice(work_list)
-            begin = begins.get(work[0][0], ('le', 'la'))
-            v = choice(len(order), 1, p=[0.6, 0.4])
-            who_first = order[choice(len(order), 1, p=[0.6, 0.4])[0]]
-            
-            if begin[0] == 'l\'':
-                if work[0] == work[1]:
-                    work_str = '{} {}'.format(begin[0], work[0])
-                else:
-                    work_str = "{}{}, {}{}".format(begin[who_first[0]],
-                                                   work[who_first[0]],
-                                                   begin[who_first[1]],
-                                                   work[who_first[1]])
+        if i == len(work_list):
+            i = 0
+            random.shuffle(work_list)
+        
+        work = random.choice(work_list)
+        begin = begins.get(work[0][0], ('le', 'la'))
+        v = choice(len(order), 1, p=[0.6, 0.4])
+        who_first = order[choice(len(order), 1, p=[0.6, 0.4])[0]]
+        
+        if begin[0] == 'l\'':
+            if work[0] == work[1]:
+                work_str = '{} {}'.format(begin[0], work[0])
             else:
-                work_str = "{} {}, {} {}".format(begin[who_first[0]],
-                                                 work[who_first[0]],
-                                                 begin[who_first[1]],
-                                                 work[who_first[1]])
+                work_str = "{}{}, {}{}".format(begin[who_first[0]],
+                                               work[who_first[0]],
+                                               begin[who_first[1]],
+                                               work[who_first[1]])
+        else:
+            work_str = "{} {}, {} {}".format(begin[who_first[0]],
+                                             work[who_first[0]],
+                                             begin[who_first[1]],
+                                             work[who_first[1]])
 
-            work_str = 'L' + work_str[1:]
+        work_str = 'L' + work_str[1:]
 
-            tweet_text = work_str + tweet_template.format(ends[who_first[0]],
-                                                          ends[who_first[1]])
-            print(tweet_text)
+        tweet_text = work_str + tweet_template.format(ends[who_first[0]],
+                                                      ends[who_first[1]])
+        tweet_text += " " + random.choice(hastags)
+
+        print(tweet_text)
+        try:
             api.update_status(tweet_text)
             sleep(sleep_time)
         except tweepy.TweepError as e:
             print(e.reason)
             sleep(2)
-
+        i += 1
     
